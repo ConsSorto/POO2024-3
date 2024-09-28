5d43408a5d6a69b8687c3816956ec012d75e0f11
Cons Sorto <cos.sorto@gmail.com>
2024-09-28 15:29:03 -0600

    demostracion de funciones

---
+++ Codigo Inicial/funciones.py
@@ -0,0 +1,21 @@
+def nombreFuncion():
+    print("Hola esta es una funcion")
+
+nombreFuncion()
+
+def sindefinircuerpo():
+    pass
+
+sindefinircuerpo()
+
+def funcionConParametro(entero1, entero2):
+    return entero1 + entero2
+
+print(funcionConParametro(1,2))
+
+
+sumarEnteros = lambda entero1, entero2: print(entero1 + entero2)
+
+sumarEnteros(100,200)
+
+
